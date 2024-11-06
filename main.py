d959c522fcd89f98fc594e0b47b626b2386ae878
Christian Baumann <christian.baumann@maibornwolff.de>
2024-11-06 08:20:58 +0100

    Move browser window to the screen left of the main screen.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import time
 import win32com.client
 from playwright.sync_api import sync_playwright
 from luxafor import Luxafor
+import win32api
+import win32gui
 
 # Load Config
 with open('config.json') as config_file:
@@ -44,11 +46,23 @@ def start_pomodoro_timer(page):
 def wait_for_timer_completion(duration, buffer_time):
     time.sleep(duration * 60 + buffer_time)
 
+def move_window_to_left_screen():
+    hWnd = win32gui.GetForegroundWindow()
+    monitors = win32api.EnumDisplayMonitors()
+    if len(monitors) > 1:
+        main_monitor = monitors[0][2]
+        left_monitor = monitors[1][2]
+        new_x = left_monitor[0]
+        new_y = main_monitor[1]
+        win32gui.SetWindowPos(hWnd, 0, new_x, new_y, 0, 0, win32con.SWP_NOSIZE | win32con.SWP_NOZORDER)
+
 with sync_playwright() as p:
     Luxafor.set_color('red', config['luxafor_id'])
     browser = p.firefox.launch(headless=False)
     context = browser.new_context(viewport={'width': 1280, 'height': 1024}, device_scale_factor=float(config['zoom_level']))
     page = context.new_page()
+
+    move_window_to_left_screen()
 
     if start_pomodoro_timer(page):
         create_appointment(config['meeting_subject'], config['attendees'], config['appointment_duration_minutes'])
